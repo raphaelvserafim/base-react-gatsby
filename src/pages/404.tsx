25f506e46a54659bb13df76b27c796372aa64765
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-07 23:31:41 -0400

    updates

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,30 @@
-import * as React from "react";
-import Container from '@mui/material/Container';
-import Layout from '../components/Layout';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
+//import img404 from '../images/error-404.svg';
 
-const NotFoundPage = () => {
+function NotFound() {
+
   return (
-    <Layout>
-      <Container sx={{ paddingTop: 10, minHeight: '100vh' }}>
-        404
-      </Container>
-    </Layout>
-  );
-};
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Box sx={{ maxWidth: 600, width: "100%" }}>
+        <Paper sx={{ padding: 2 }}>
+          <h1 style={{ textAlign: "center" }}>Page not found 😅 </h1>
+          <Grid container justifyContent="center" alignItems="center" xs={12} sm={12} md={12}>
+            {/* <img style={{ maxWidth: 400, width: "100%" }} loading="lazy" src={img404} alt="404" /> */}
+          </Grid>
+          <p style={{ textAlign: "center" }}>we could not find this page</p>
+          <Grid container justifyContent="center" alignItems="center" xs={12} sm={12} md={12}>
+            <Button variant="outlined">Back Home</Button>
+          </Grid>
+        </Paper>
+      </Box>
+    </Box>
+  )
 
-export default NotFoundPage;
+}
 
-export const Head = () => <title>404</title>;
+export default NotFound;
