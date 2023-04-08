b2836341faa487550f3150f87a15c262f1df10c0
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-07 22:43:01 -0400

    update: CardSkeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+
 import Layout from '../components/Layout';
 
+import CardSkeleton from "../components/CardSkeleton";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Container sx={{ paddingTop: 10, minHeight: '100vh' }}>
-        Home
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0} >
+            {
+              [...Array(6)].map((_, index) => (
+                <Grid key={index} xs={12} sm={6} md={4}>
+                  <CardSkeleton loading={true} />
+                </Grid>
+              ))
+            }
+          </Grid>
+        </Box>
       </Container>
     </Layout>
   );
