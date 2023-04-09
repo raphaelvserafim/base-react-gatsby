2684cf273aa775bbbc3e9ad8e656971d7b3b3462
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-08 21:52:09 -0400

    update: firebase

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,22 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, doc, deleteDoc, getDocs, } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
- 
 
-import dotenv from 'dotenv';
-dotenv.config();
+import { firebaseConfig } from "./config";
 
+try {
+    initializeApp(firebaseConfig);
+} catch (error) {
+    console.log("error init firebase", error);
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-};
-const app = initializeApp(firebaseConfig);
-
+}
 
 const auth = getAuth();
 const db = getFirestore();
