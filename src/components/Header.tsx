b94df10f1f6c55323c77c0ab869566c0baf1d06a
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-09 11:34:42 -0400

    feat: logout and is user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,44 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { navigate } from 'gatsby';
 import { Link } from 'gatsby';
+ 
+
 
 import Menu from './Menu';
 import ButtonIsLogged from "./ButtonIsLogged"
 
 import { APP_NAME } from '../config';
 
-const log = 0;
 
 import "../css/App.css"
 
+import { isUser } from '../Auth';
+
+
 const Header = () => {
 
-    if (log) {
-        var ButtonLogin = <ButtonIsLogged />
+    const [isLogged, setIsLogged] = React.useState(false);
+    const [user, setUser] = React.useState({});
+
+    React.useEffect(() => {
+        const checkUser = async () => {
+            try {
+                const user = await isUser();
+                if (user) {
+                    setIsLogged(true);
+                    setUser(user)
+                    console.log("isLogged")
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        checkUser();
+
+    }, []);
+
+    if (isLogged) {
+        var ButtonLogin = <ButtonIsLogged user={user} />
     } else {
         var ButtonLogin = <Button onClick={() => { navigate(`/login`) }} color="inherit">Login</Button>;
     }
