b1791f800f07cc3f67bc1ed7c7b1ac5446c7e941
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-08 20:55:22 -0400

    feat: firebase

---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+// Use o firebaseConfig onde for necessário
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
