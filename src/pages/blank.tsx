b1f1f714cd4091a48f35dc7e2c699638c1c585a4
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-10 23:31:40 -0400

    feat: Blank

---
+++ src/pages/blank.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Container from '@mui/material/Container';
+import { Helmet } from 'react-helmet';
+
+import Layout from '../components/Layout';
+
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <Container sx={{ paddingTop: 10, minHeight: '100vh' }}>
+            Blank
+            </Container>
+        </Layout>
+    );
+};
+
+export default IndexPage;
+
+
+export const Head = () => (
+    <Helmet>
+        <title>Blank</title>
+        <meta name="description" content="Este é o login da minha página" />
+        <meta name="keywords" content="login, autenticação, página de login" />
+        <meta name="author" content="Seu nome ou nome da empresa" />
+        <meta property="og:title" content="Login" />
+        <meta property="og:description" content="Este é o login da minha página" />
+        <meta property="og:image" content="https://www.example.com/image.jpg" />
+        <meta property="og:url" content="https://www.example.com/login" />
+        <meta name="twitter:title" content="Login" />
+        <meta name="twitter:description" content="Este é o login da minha página" />
+        <meta name="twitter:image" content="https://www.example.com/image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+    </Helmet>
+);
