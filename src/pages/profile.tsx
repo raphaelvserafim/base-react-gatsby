60539d4e11712dc0536eedae6b6c00c67c2235f7
Raphael Serafim <raphaelvserafim@gmail.com>
2023-04-10 23:33:22 -0400

    feat: Profile

---
+++ src/pages/profile.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+
+import Layout from '../components/Layout';
+ 
+const ProfilePage = () => {
+    return (
+        <Layout>
+            <Container sx={{ paddingTop: 10, minHeight: '100vh' }}>
+
+
+            </Container>
+        </Layout>
+    );
+};
+
+export default ProfilePage;
+
+export const Head = () => <title>Profile</title>;
